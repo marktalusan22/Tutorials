dfa10eb859ba896e9668239a3675b12505019428
Mark Talusan <markjosephtalusan@gmail.com>
2026-02-09 19:29:17 +0800

    LoopDemo.java

---
+++ LoopDemo.java
@@ -0,0 +1,15 @@
+public class LoopDemo {
+    public static void main(String[] args) {
+        
+        int x = 1;
+        int y = 0;
+        for (x=1; x<=2; x++)
+        {
+            for (y=1; y<=3; y++)
+            {
+      
+        System.out.println(x + " " + y);
+            }
+        }
+    }
+}
